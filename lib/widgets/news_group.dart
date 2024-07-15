b0c8cd005793d419be737813dd814877192042a7
aymen gasmi <aymengasmi25@gmail.com>
2024-07-15 10:29:10 +0100

    trigger getnews method and display news

--- lib/widgets/news_group.dart
+++ lib/widgets/news_group.dart
@@ -1,19 +1,28 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:my_news/services/news_service.dart';
 import 'package:my_news/widgets/news_item.dart';
 import '../models/news_model.dart';
 
 class NewsGroup extends StatefulWidget {
-
   NewsGroup({super.key});
 
   @override
   State<NewsGroup> createState() => _NewsGroupState();
 }
 
-
-
 class _NewsGroupState extends State<NewsGroup> {
-  final List<NewsModel> news = [];
+  List<NewsModel> news = [];
+  @override
+  void initState() {
+    super.initState();
+    getGeneralNews();
+  }
+
+  Future<void> getGeneralNews() async {
+    news = await NewsService(Dio()).getNews();
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
