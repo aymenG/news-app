738d1cad6dff6083e2a2c16982920fde076aba4d
aymen gasmi <aymengasmi25@gmail.com>
2024-08-06 09:59:59 +0100

    create category view

---
+++ lib/views/category_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:my_news/widgets/category_group.dart';
+import 'package:my_news/widgets/news_group.dart';
+
+class CategoryView extends StatelessWidget {
+  const CategoryView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(), body: SizedBox());
+  }
+}
--- lib/widgets/category_card.dart
+++ lib/widgets/category_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:my_news/models/category_model.dart';
+import 'package:my_news/views/category_view.dart';
 
 class CategoryCard extends StatelessWidget {
   final CategoryModel categoryModel;
@@ -10,24 +11,31 @@ class CategoryCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-      decoration: BoxDecoration(
-          shape: BoxShape.rectangle,
-          borderRadius: BorderRadius.circular(12),
-          image: DecorationImage(
-            image: AssetImage(categoryModel.image),
-            fit: BoxFit.cover,
-          )),
-      height: 100,
-      width: 180,
-      child: Center(
-        child: Text(
-          categoryModel.name,
-          style: const TextStyle(
-            color: Colors.yellow,
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+          return const CategoryView();
+        }));
+      },
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+        decoration: BoxDecoration(
+            shape: BoxShape.rectangle,
+            borderRadius: BorderRadius.circular(12),
+            image: DecorationImage(
+              image: AssetImage(categoryModel.image),
+              fit: BoxFit.cover,
+            )),
+        height: 100,
+        width: 180,
+        child: Center(
+          child: Text(
+            categoryModel.name,
+            style: const TextStyle(
+              color: Colors.yellow,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
           ),
         ),
       ),
